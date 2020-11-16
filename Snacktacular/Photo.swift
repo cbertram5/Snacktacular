7fbf9d4f7c72e55fa44c104fa0fe210b4c8a6776
Chris Bertram <bertramc@bc.edu>
2020-11-16 00:41:21 -0500

    8.22 complete

--- Snacktacular/Photo.swift
+++ Snacktacular/Photo.swift
@@ -47,5 +47,57 @@ class Photo {
         let photoURL = dictionary["photoURL"] as! String? ?? ""
         self.init(image: UIImage(), description: description, photoUserID: photoUserID, photoUserEmail: photoUserEmail, date: date, photoURL: photoURL, documentID: "")
     }
-
+    
+    func saveData(spot: Spot, completion: @escaping (Bool) -> ()) {
+        let db = Firestore.firestore()
+        let storage = Storage.storage()
+        
+        // coonvert photo.image o a Data type so that it can be saved in Firebase Storage
+        guard let photoData = self.image.jpegData(compressionQuality: 0.5) else {
+            print("ERROR: could not convert photo.image to Data.")
+            return
+        }
+        
+        // create metadata so that we can see images in the Firebase Store Console
+        let uploadMetaData = StorageMetadata()
+        uploadMetaData.contentType = "image/jpeg"
+        
+        // create file name if necessary
+        if documentID == "" {
+            documentID = UUID().uuidString
+        }
+        
+        // create a storage reference to upload this image file to the spot's folder
+        let storageRef = storage.reference().child(spot.documentID).child(documentID)
+        
+        // create an uploadTask
+        let uploadTask = storageRef.putData(photoData, metadata: uploadMetaData) { (metadata, error) in
+            if let error = error {
+                print("ERROR: upload for ref  \(uploadMetaData) failed. SOMETHING ELSE I COULD NOT LOAD \(error.localizedDescription)")
+            }
+        }
+        
+        uploadTask.observe(.success) { (snapshot) in
+            print("Upload to Firebase Storage was succesful")
+            
+            // create the dictionary representing data we want to save
+            let dataToSave = self.dictionary
+            let ref = db.collection("spots").document(spot.documentID).collection("photos").document(self.documentID)
+            ref.setData(dataToSave) { (error) in
+                guard error == nil else {
+                    print("😡 ERROR: updating document \(error!.localizedDescription)")
+                    return completion(false)
+                }
+                print("🥳 updated document: \(self.documentID) in spot: \(spot.documentID)") // it WORKED!
+                completion(true)
+            }
+        }
+        
+        uploadTask.observe(.failure) { (snapshot) in
+            if let error = snapshot.error {
+                print("ERROR: upload task for file \(self.documentID) failed, in spot \(spot.documentID), with error \(error.localizedDescription)")
+            }
+            completion(false)
+        }
+    }
 }
--- Snacktacular/PhotoViewController.swift
+++ Snacktacular/PhotoViewController.swift
@@ -70,6 +70,11 @@ class PhotoViewController: UIViewController {
         }
     }
     
+    func updateFromUserInterface() {
+        photo.description = descriptionTextView.text!
+        photo.image = photoImageView.image!
+    }
+    
     func addBordersToEditableObjects() {
         descriptionTextView.addBorder(width: 0.5, radius: 5.0, color: .black)
     }
@@ -90,7 +95,13 @@ class PhotoViewController: UIViewController {
         leaveViewController()
     }
     @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
-        //TODO more setup
-        leaveViewController()
+        updateFromUserInterface()
+        photo.saveData(spot: spot) { (success) in
+            if success {
+                self.leaveViewController()
+            } else {
+                print("ERROR")
+            }
+        }
     }
 }
